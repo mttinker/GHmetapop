e82151d4f842d572e9298f51531d9acd3d22fb51
mttinker <ttinker@nhydra.com>
2018-02-19 21:04:09 -0400

    Calculate variaiton in density by habitat variables

--- GHmetapop.R
+++ GHmetapop.R
@@ -10,8 +10,8 @@ MnImLo = 1           # Mean annual immigrants with low immigration option
 MnImHi = 3           # Mean annual immigrants with high immigration option
 V_mn = 2.5           # Population front asymptotic wavespeed, km/yr, min  
 V_mx = 5.5           # Population front asymptotic wavespeed, km/yr, max
-K_mean = 3.5         # Baseline mean K (can modify as fxn of habitat variables)
-K_sig = 2            # Standard deviation in local K (variation over space)
+K_mean = 3           # Baseline mean K (can modify as fxn of habitat variables)
+K_sig = 1.5          # Standard deviation in local K (variation over space)
 sig = 0.05           # Environmental stochasticity (std dev in log-lambda)
 rmax = log(1.22)     # Maximin rate of growth = 22% per year
 theta = 1            # theta parameter for theta-logistic (1 = Ricker model, >1 = delayed DD) 
@@ -25,10 +25,14 @@ library(gtools)
 library(BMS)
 library(boot)
 library(ggplot2)
+library(ggmap)
 library(reshape2)
+library(rgdal)
+library(raster)
 #
 # Load files --------------------------------------------------------------
 data = read.csv("GHBlockdata.csv", header = TRUE)
+Cdata = read.csv("GHCelldata.csv", header = TRUE)
 Demdat = read.csv("RandDem.csv", header = TRUE)
 Distmat =  read.csv("Distmat.csv", header = FALSE)
 # Probabilities of dispersal from each block for each age/sex class
@@ -39,9 +43,11 @@ destJF = read.csv("GHDispMatJF.csv", header = FALSE)
 destAF = read.csv("GHDispMatAF.csv", header = FALSE);
 destJM = read.csv("GHDispMatJM.csv", header = FALSE);
 destAM = read.csv("GHDispMatAM.csv", header = FALSE);
+Habavg = read.csv("HabAvg.csv", header = TRUE);
+# HAIDA <- raster("HGland.grd") # Raster of Gwaii Haanas, NAD_1983_Albers (BC Environment)
 #
 # Process data ----------------------------------------------------------------
-Dispers = 3  # Over-Dispersion param for Neg Binomial # immigrants per year
+Dispers = 2.5  # Over-Dispersion param for Neg Binomial # immigrants per year
 pparLo = Dispers/(Dispers+MnImLo/length(Initblk))
 pparHi = Dispers/(Dispers+MnImHi/length(Initblk))
 Years = c(Yr1:(Yr1+Nyrs-1))  
@@ -52,6 +58,50 @@ N0 = numeric(length = P)
 for (i in 1:length(Initblk)){
   N0[Initblk[i]] = round(Initpop)/length(Initblk)
 }
+#  Create Data variables for calculating relative density of Hab cells:
+PUID = Cdata$PU_ID
+Blk = Cdata$BlockID
+area = Cdata$Area
+dep = Cdata$DEPTH
+fetch = Cdata$Fetch
+botm = as.character(Cdata$BT_Code)
+# Load params for habitat density at K fxn
+params = read.csv("Hab_params.csv")
+parms = params$Parms
+# Define Kcalc function
+Kcalc <- function(PUID,Blk,area,dep,botm,fetch,parms,Kmn){
+  b <- parms  
+  # Hab dens multiplier fxn: exp(b1*X1 + b2*X2... + bn*Xn)
+  # Depth part of fxn: b1*(-1*dep) - b2*dep^2
+  # where sum(area*exp(b1*(-1*dep)-b2*(dep^2)))/sum(area) =~ 1
+  Depfxn = b[1]*(-1*dep) - b[2]*dep^2
+  # Fetch part of fxn
+  fch = fetch - mean(fetch)
+  Fchfxn = b[3]*(fch) 
+  btfxn = numeric(length = length(botm))
+  btfxn[which(botm=="1")] = b[4]; btfxn[which(botm=="1a")] = b[5]; btfxn[which(botm=="1b")] = b[6]
+  btfxn[which(botm=="2")] = b[7]; btfxn[which(botm=="2a")] = b[8]; btfxn[which(botm=="2b")] = b[9]
+  btfxn[which(botm=="3")] = b[10]; btfxn[which(botm=="3a")] = b[11]; btfxn[which(botm=="3b")] = b[12]
+  btfxn[which(botm=="0")] = b[13]
+  mult = exp(Depfxn + Fchfxn + btfxn) 
+  # NOTE: sum((mult*area))/sum(area) should equal approx 1
+  Kdns = numeric(length = max(Blk))
+  Ktot = numeric(length = max(Blk))
+  AreaB = numeric(length = max(Blk))
+  for (i in 1:max(Blk)){
+    ii = which(Blk == i)
+    Kdns[i] = mean(Kmn*mult[ii])
+    Ktot[i] = sum(Kmn*area[ii]*mult[ii])
+    AreaB[i] = sum(area[ii])
+    Kdns[i] = Ktot[i]/sum(area[ii])
+  }
+  Habdns = data.frame(PUID = PUID, Reldens = mult)
+  Ktab = data.frame(Block = seq(1:max(Blk)), Area = AreaB, 
+                    Kdns = Kdns, Ktot = Ktot)
+  result <- list(Ktab=Ktab,Habdns=Habdns)
+  return(result)
+}
+# 
 # Dispersal probabilities
 disp = matrix(data = NA,nrow = 4, ncol = P)
 disp[1,] = DispP$Jf
@@ -61,16 +111,10 @@ disp[4,] = DispP$Am
 #
 # Estimate K for each Block: 
 # NOTE: this will eventually be a function of habitat parameters
-Kmn = numeric(length=P)
-Ksd = numeric(length=P)
-KV = numeric(length=P)
-Areahab = numeric(length=P)
-for(i in 1:P){
-  Areahab[i] = data$Area[i] 
-  Kmn[i] = K_mean
-  Ksd[i] = K_sig
-  KV[i] = Ksd[i]^2
-}
+tmp = Kcalc(PUID,Blk,area,dep,botm,fetch,parms,K_mean); Ktab=tmp$Ktab; Habdns=tmp$Habdns
+Kmn = Ktab$Kdns; KV = K_sig^2
+muK = log(Kmn/sqrt(1+KV/Kmn^2)); sigK = sqrt(log(1+KV/Kmn^2))
+Areahab = Ktab$Area
 # *Create a default matrix with lambda ~1.22 (rmax)
 A = matrix(c(
   0.7176,    0.4608,    0,         0,
@@ -91,6 +135,7 @@ N = array(data = 0, c(P,Nyrs,reps))
 Nmn = matrix(0,nrow=P,ncol=Nyrs)
 Nmn[,1] = N0
 zvec = matrix(data = 0,nrow = 4, ncol = 1)
+
 # Cycle through reps
 for (r in 1:reps){
   # Number of years of population establishment (before range expansion begins)
@@ -106,7 +151,12 @@ for (r in 1:reps){
   n = array(data = 0, c(4,P,Nyrs))
   nt = matrix(data = 0,nrow = 4, ncol = 1) 
   nd = matrix(data = 0,nrow = 4, ncol = 1)
+  # If uncertainty in params, include here:
+  # tmp = Kcalc(PUID,Blk,area,dep,botm,fetch,parms,KDmean); Ktab=tmp$Ktab; Kmn = Ktab$Kdns
+  # muK = log(Kmn/sqrt(1+KV/Kmn^2)); sigK = sqrt(log(1+KV/Kmn^2))
+  K = numeric(length = P)
   for (i in 1:P){
+    K[i] = rlnorm(1,muK[i],sigK[i])*Areahab[i]
     # n[1:4, i, 1] = rmultinom(1, N0[i], sad) 
     if(N0[i]>0){
       n[1:4, i, 1] =c(0,2,0,0)+ rmultinom(1, N0[i]-2, c(.1,0,.4,.5))
@@ -114,8 +164,6 @@ for (r in 1:reps){
     }else{
       n[1:4, i, 1] = c(0,0,0,0)
     }
-    KDmean = rlnorm(1,log(Kmn[i]/sqrt(1+KV[i]/Kmn[i]^2)),sqrt(log(1+KV[i]/Kmn[i]^2)))
-    K[i] = KDmean*Areahab[i]
   }
   N[,1, r] = colSums(n[, , 1])
   for (y in 2:Nyrs) {
@@ -293,7 +341,7 @@ for(y in 2:Nyrs){
 }
 Pop_Overall <- data.frame(Year=Years,Mean=means,lower=Lo,upper=Hi,
                           SEmean=SEmean,CImeanLo=CImn[,1],CImeanHi=CImn[,2])
-write.csv(Pop_Overall,'Results_GHtot.csv')
+write.csv(Pop_Overall,'Results_GHtot.csv',row.names = FALSE)
 
 titletxt = paste0("Sea Otter Population Projection, ", Nyrs," Years")
 plt2 = (ggplot(Pop_Overall, aes(Year, Mean))+
@@ -340,4 +388,28 @@ for (p in 1:P){
 Hab_Blocks <- data.frame(Block = BlockIDs, 
                          Year=YearsALL,Mean=meansALL, 
                          lower=LoALL,upper=HiALL,Density=dfDens$Density )
-write.csv(Hab_Blocks,'Results_GHblocks.csv')
+write.csv(Hab_Blocks,'Results_GHblocks.csv',row.names = FALSE)
+
+Habdns$DensT = 0
+BlkDnsT = Hab_Blocks$Density[Hab_Blocks$Year==max(Years)]
+for (i in 1:length(Habdns$PUID)){
+  Habdns$DensT[i] = Habdns$Reldens[i]*sum(BlkDnsT*as.numeric(Habavg[i,2:(P+1)]))
+}
+write.csv(Habdns,'Results_GHcells.csv',row.names = FALSE)
+Cdata$DensT = Habdns$DensT
+
+# plot map of Results ---------------------------------------------------------
+GHland<-readOGR("GH_Land_poly.shp", layer="GH_Land_poly")
+GHland_df <- fortify(GHland)
+map <- ggplot() +
+  geom_polygon(data = GHland_df, 
+            aes(x = long, y = lat, group = group)) +
+  
+  # geom_point(aes(x=Xcoord, y=Ycoord, color=DensT), data=Cdata, alpha=1, size=1, color="grey20") +
+  geom_point(aes(x=Xcoord, y=Ycoord, color=DensT), data=Cdata, alpha=1, size=1.5, shape=15)+
+  scale_colour_gradientn("Mean Density, Final Year", 
+                         colours=c( "#f9f3c2","#660000"))+ # change color scale
+  coord_equal(ratio=1) # square plot to avoid the distortion
+print(map) 
+
+
